b1a953e175c4e0e6e949cc5477fb8f9737a77e1a
sryanyuan <sryan@qq.com>
2018-02-02 10:27:24 +0800

    comment

--- ttree.go
+++ ttree.go
@@ -1,12 +1,17 @@
 package ttreego
 
 type TTree interface {
+	// Add a string into trie tree
 	Add([]byte) bool
 	AddString(string) bool
+	// Remove a string from trie tree
 	Remove([]byte) error
 	RemoveString(string) error
+	// Check if a string has prefix
 	Match([]byte) bool
 	MatchString(string) bool
+	// Get the add count
 	GetCount() int
+	// Reset the trie tree
 	Reset()
 }
